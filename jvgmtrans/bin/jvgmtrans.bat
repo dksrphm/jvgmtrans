bb24edf8f6091a02264174dd0b60838e620796f1
dksrphm <dksrphm>
2022-04-19 21:28:14 +0900

    [remove] ref #3 bin/jvgmtrans.bat
    Not work. Please use /jvgmtrans.bat

--- jvgmtrans/bin/jvgmtrans.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set VGMFILE="%1"
-echo %VGMFILE%
-
-java -DDEBUG -cp .;./jvgmtrans jvgmtrans/Jvgmtrans "%VGMFILE%" > jvgmtrans.log
-rem java -cp .;./jvgmtrans jvgmtrans/Jvgmtrans "%VGMFILE%" > jvgmtrans.log
-
-pause
