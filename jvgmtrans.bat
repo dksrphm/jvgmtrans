930e22782f3a101042903cfb3604b66a854f7593
dksrphm <dksrphm>
2022-04-22 00:15:14 +0900

    [fix]refs #3 To be able to exec in any directory

--- jvgmtrans.bat
+++ jvgmtrans.bat
@@ -1,10 +1,12 @@
 @echo off
+set BATPATH=%~dp0
+
 if "%1"== "" goto USAGE
 
 set VGMFILE=%1
 echo %VGMFILE%
 
-java -DDEBUG -classpath .;./bin jvgmtrans.Jvgmtrans "%VGMFILE%" > jvgmtrans.log
+java -DDEBUG -classpath "%BATPATH%/bin" jvgmtrans.Jvgmtrans "%VGMFILE%" > jvgmtrans.log
 
 rem pause
 exit /b
